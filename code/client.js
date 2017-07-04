4617e0ba901947baed567ec3586eb5400774739b
David Smith <daveonhols2003@yahoo.co.uk>
2017-07-04 20:46:31 +0100

    first reasonable version

---
+++ code/client.js
@@ -0,0 +1,12 @@
+
+const Twitter = require("twitter");
+const Secrets = require("../secrets/secret.js");
+
+const client = new Twitter({
+  consumer_key: Secrets.getSecret("consumer_key"),
+  consumer_secret: Secrets.getSecret("consumer_secret"),
+  access_token_key: Secrets.getSecret("access_token_key"),
+  access_token_secret: Secrets.getSecret("access_token_secret")
+});
+
+exports.client = client;
---
+++ test/testeventhandler.js
@@ -0,0 +1,51 @@
+"use strict";
+
+const expect = require("chai").expect;
+
+const urlCounter = require("./../code/objcounter.js").OCounter;
+const eventHandler = require("./../code/eventhandler.js").handleTweetEvent;
+const store = require("./../code/arraystore.js");
+
+it("Add with store", function() {
+
+
+  const storage = [];
+
+  const storage_provider = store.getArrayStorageProvider(storage);
+  const counter = new urlCounter([], storage_provider);
+
+  eventHandler(counter, "tweet_1", "9001", "david", [{ expanded_url: "http://exp.url.com/one" }]);
+  expect(storage.length).to.equal(1);
+  expect(storage[0].value.length).to.equal(1);
+  expect(storage[0].key.url).to.equal("http://exp.url.com/one");
+  expect(storage[0].value[0].tweet.text).to.equal("tweet_1");
+
+  eventHandler(counter, "tweet_2", "9002", "david", [{ expanded_url: "http://exp.url.com/one" }]);
+  expect(storage.length).to.equal(1);
+  expect(storage[0].value.length).to.equal(2);
+  expect(storage[0].key.url).to.equal("http://exp.url.com/one");
+  expect(storage[0].value[0].tweet.text).to.equal("tweet_1");
+  expect(storage[0].value[1].tweet.text).to.equal("tweet_2");
+
+  eventHandler(counter, "tweet_3", "9003", "david", [{ expanded_url: "http://exp.url.com/two" }]);
+  expect(storage.length).to.equal(2);
+  expect(storage[0].value.length).to.equal(1);
+  expect(storage[0].key.url).to.equal("http://exp.url.com/two");
+  expect(storage[0].value[0].tweet.text).to.equal("tweet_3");
+  expect(storage[1].value.length).to.equal(2);
+  expect(storage[1].key.url).to.equal("http://exp.url.com/one");
+  expect(storage[1].value[0].tweet.text).to.equal("tweet_1");
+  expect(storage[1].value[1].tweet.text).to.equal("tweet_2");
+
+  eventHandler(counter, "tweet_1", "9001", "david", [{ expanded_url: null }]);
+  expect(storage.length).to.equal(2);
+  expect(storage[0].value.length).to.equal(1);
+  expect(storage[0].key.url).to.equal("http://exp.url.com/two");
+  expect(storage[0].value[0].tweet.text).to.equal("tweet_3");
+  expect(storage[1].value.length).to.equal(2);
+  expect(storage[1].key.url).to.equal("http://exp.url.com/one");
+  expect(storage[1].value[0].tweet.text).to.equal("tweet_1");
+  expect(storage[1].value[1].tweet.text).to.equal("tweet_2");
+
+})
+
